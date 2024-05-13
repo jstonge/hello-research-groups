695cc0280e03453187cf4bcee747ab6c410e377a
Jonathan St-Onge <35715881+jstonge@users.noreply.github.com>
2024-05-13 18:20:56 -0400

    Create comp-taxonomy.py

---
+++ data/raw/comp-taxonomy.py
@@ -0,0 +1,43 @@
+# Computational_Works_Taxonomy_Version: 2024-05-13
+computational
+computational > foss
+computational > foss > data availability
+computational > foss > data availability > dryad
+computational > foss > code availability
+computational > foss > code availability > git
+computational > foss > code availability > gitlab
+computational > foss > code availability > osf
+computational > foss > programming language
+computational > foss > programming language > python
+computational > foss > programming language > R
+computational > foss > programming language > julia
+computational > foss > programming language > stan
+computational > empirical
+computational > empirical > methods
+computational > empirical > methods > BERT
+computational > empirical > methods > embeddings
+computational > empirical > methods > topic modeling
+computational > empirical > methods > topic modeling  > BERT
+computational > empirical > methods > topic modeling  > LDA
+computational > empirical > social media
+computational > empirical > social media > facebook
+computational > empirical > social media > reddit
+computational > empirical > social media > twitter
+computational > simulation
+computational > simulation > agent-based modeling
+computational > simulation > bayesian
+computational > simulation > generative process
+computational > simulation > mcmc
+computational > simulation > mcmc > gibbs sampling
+computational > simulation > mcmc > hmc
+computational > machine learning
+computational > machine learning > benchmarking
+computational > theory 
+computational > theory > algorithm
+conceptual
+conceptual > editorial
+conceptual > essay
+conceptual > review
+conceptual > best practices
+theory
+theory > mathematical
